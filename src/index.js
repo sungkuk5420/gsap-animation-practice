2d69575735ee6df8a774b73167eb15e304765f06
kim_mindfree <kim_sungkuk@mindfree.co.jp>
2021-01-13 01:54:55 +0900

    hover effect 추가 및 스타일링

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import hoverEffect from 'hover-effect';
+const aa =new hoverEffect({
+    parent:document.querySelector('.distortion'),
+    intensity:0.2,
+    image1:"./images/01.png",
+    image2:"./images/02.png",
+    displacementImage:"./images/diss.png"
+})
+
+console.log(aa)
